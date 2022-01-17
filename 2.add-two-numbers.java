79433c6e96af4ed2fb5dd6b9cfc0a7f25828934b
ianhuang <ianaa10716@gmail.com>
2022-01-17 08:15:07 +0800

    新增leetcode答題

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carr = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            // 计算对应位置上两个数相加之和 + 进位
+            int sum = x + y + carr;	
+            // 计算进位的值
+            carr = sum / 10;
+            // 利用链表的 尾插法 插入数据
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carr != 0) {
+            curr.next = new ListNode(carr);
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
